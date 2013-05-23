6509e930059062839ff533dfa88251e9ca1b9d68
Apprentice <apprentice@devbootcamp.com>
2013-05-22 19:08:12 -0700

    intital commit, model needs refactor

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,45 @@
+class TodosController < ApplicationController
+  before_filter :load_todos
+
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def show
+    @todo = Todo.find params[:id]
+  end
+
+  def create
+    @todo = Todo.new params[:todo]
+    if @todo.save
+      @todo.update_count
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @todo = Todo.find params[:id]
+  end
+
+  def update
+    @todo = Todo.find params[:id]
+    if @todo.update_attributes params[:todo]
+      @todo.update_count
+      redirect_to @todo
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def load_todos
+    @todos = Todo.all
+  end
+end
